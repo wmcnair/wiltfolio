2efc60b57ca509d3f52dcbf4e9e674c03c7b6561
SBTC Webmin <willymac601@gmail.com>
2019-09-26 15:45:26 -0500

    myfolio start

--- script/wilt_script.js
+++ script/wilt_script.js
@@ -1,5 +1,22 @@
 // Script Code
 $(document).ready(function() {
+    // ------- WOW ANIMATED ------ //
+    var wow = new WOW(
+        {
+          boxClass:     'wow',      // animated element css class (default is wow)
+          animateClass: 'animated', // animation css class (default is animated)
+          offset:       0,          // distance to the element when triggering the animation (default is 0)
+          mobile:       false,       // trigger animations on mobile devices (default is true)
+          live:         true,       // act on asynchronously loaded content (default is true)
+          callback:     function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+          },
+          scrollContainer: null,    // optional scroll container selector, otherwise use window,
+          resetAnimation: true,     // reset animation on end (default is true)
+        }
+    );
+    wow.init();
 
     var topTxt = $('#home .intro');
     var topWrap = $('#home .jumbotron-fluid');
@@ -14,6 +31,18 @@ $(document).ready(function() {
             topWrap.prepend(topTxt);
         }
     });
+
+    /* $('.fade-in-left').bind('inview', function (event, visible) {
+        if (visible) {
+            $(this).addClass('animated fadeInLeft');
+        }  
+    });
+
+    $('.fade-in-right').bind('inview', function (event, visible) {
+        if (visible) {
+            $(this).addClass('animated fadeInRight');
+        }  
+    }); */
 
     /*$(window).resize(function(){
         if ($(window).width() <= 768){
